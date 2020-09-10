357f994f7c9eaef9f7c9eb1f28ea6192c89e74c6
JSReds <andrea.rosci90@gmail.com>
2020-09-10 12:12:46 +0200

    karmaConfig: fix tests against node 10
    fix tests against node 10
    
    Change-type: patch
    Signed-off-by: Andrea Rosci <andrear@balena.io>

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,10 @@
 var getKarmaConfig = require('balena-config-karma');
 var packageJSON = require('./package.json');
 
+getKarmaConfig.DEFAULT_WEBPACK_CONFIG.externals = {
+	fs: true
+};
+
 module.exports = function(config) {
   var karmaConfig;
   karmaConfig = getKarmaConfig(packageJSON);
