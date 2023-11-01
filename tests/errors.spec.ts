0ac1bb548d59a4e293070918932da30c664e975e
Thodoris Greasidis <thgreasi@gmail.com>
2023-11-01 10:38:04 +0200

    Drop mochainon
    Change-type: patch

--- tests/errors.spec.ts
+++ tests/errors.spec.ts
@@ -1,4 +1,4 @@
-import * as m from 'mochainon';
+import { expect } from 'chai';
 import * as errors from '..';
 
 interface BalenaErrorClass {
@@ -19,35 +19,33 @@ const runForAllErrors = (
 describe('Errors, TS:', function () {
 	it('should expose only Error instances', () => {
 		runForAllErrors((_errorName, ErrorClass) => {
-			m.chai.expect(ErrorClass.prototype).to.be.an.instanceof(Error);
+			expect(ErrorClass.prototype).to.be.an.instanceof(Error);
 		});
 	});
 
 	it('should have a code string equal to the name', () => {
 		runForAllErrors((errorName, ErrorClass) => {
-			m.chai.expect(ErrorClass.prototype.code).to.equal(errorName);
+			expect(ErrorClass.prototype.code).to.equal(errorName);
 		});
 	});
 
 	it('should have number exit codes', () => {
 		runForAllErrors((_errorName, ErrorClass) => {
-			m.chai.expect(ErrorClass.prototype.exitCode).to.be.a('number');
+			expect(ErrorClass.prototype.exitCode).to.be.a('number');
 		});
 	});
 
 	it('should have exit codes not equal to zero', () => {
 		runForAllErrors((_errorName, ErrorClass) => {
-			m.chai.expect(ErrorClass.prototype.exitCode).to.not.equal(0);
+			expect(ErrorClass.prototype.exitCode).to.not.equal(0);
 		});
 	});
 
 	it('should generate usable errors', () => {
 		runForAllErrors((_errorName, ErrorClass) => {
-			m.chai
-				.expect(() => {
-					throw new ErrorClass();
-				})
-				.to.throw(ErrorClass);
+			expect(() => {
+				throw new ErrorClass();
+			}).to.throw(ErrorClass);
 		});
 	});
 });
